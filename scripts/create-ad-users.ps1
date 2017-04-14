f8517f0542f6740b46985a836739c0ed5dd82498
Joshua Davis <me@jdav.is>
2017-04-13 23:28:55 -0400

    added remote execution script for powershell

--- scripts/create-ad-users.ps1
+++ scripts/create-ad-users.ps1
@@ -1,5 +1,4 @@
 $password = ConvertTo-SecureString -String 'Pass@word!' -AsPlainText -Force
-#New-ADUser -Name 'Jim Smith' -GivenName 'Jim' -Surname 'Smith' -SamAccountName 'jim.smith' -UserPrincipalName 'jim.smith@azurelab.cloud' -AccountPassword $password -PassThru | Enable-ADAccount
-#New-ADUser -Name 'Sally Holly' -GivenName 'Sally' -Surname 'Holly' -SamAccountName 'sally.holly' -UserPrincipalName 'sally.holly@azurelab.cloud' -AccountPassword $password -PassThru | Enable-ADAccount
-#New-ADUser -Name 'Richard Jackson' -GivenName 'Richard' -Surname 'Jackson' -SamAccountName 'richard.jackson' -UserPrincipalName 'richard.jackson@azurelab.cloud' -AccountPassword $password -PassThru | Enable-ADAccount
-New-ADUser -Name 'Joshua Davis' -GivenName 'Joshua' -Surname 'Davis' -SamAccountName 'joshua.davis' -UserPrincipalName 'joshua.davis@azurelab.cloud' -AccountPassword $password -PassThru | Enable-ADAccount
+New-ADUser -Name 'Jim Smith' -GivenName 'Jim' -Surname 'Smith' -SamAccountName 'jim.smith' -UserPrincipalName 'jim.smith@azurelab.cloud' -AccountPassword $password -PassThru | Enable-ADAccount
+New-ADUser -Name 'Sally Holly' -GivenName 'Sally' -Surname 'Holly' -SamAccountName 'sally.holly' -UserPrincipalName 'sally.holly@azurelab.cloud' -AccountPassword $password -PassThru | Enable-ADAccount
+New-ADUser -Name 'Richard Jackson' -GivenName 'Richard' -Surname 'Jackson' -SamAccountName 'richard.jackson' -UserPrincipalName 'richard.jackson@azurelab.cloud' -AccountPassword $password -PassThru | Enable-ADAccount
