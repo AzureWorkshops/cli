58c98b8838d88240a8cd27a5139a78d11d3d4f6d
Joshua Davis <me@jdav.is>
2017-04-18 13:32:19 -0400

    updating ps scripts

---
+++ scripts/add-machine-to-ad.ps1
@@ -0,0 +1,4 @@
+$password = ConvertTo-SecureString “Pass@word1234” -AsPlainText -Force
+$creds = New-Object System.Management.Automation.PSCredential(“azurelab\cloudadmin”, $password)
+
+Add-Computer -DomainName "azurelab.cloud" -Credential $creds -Restart -Force -Confirm:$false
--- scripts/create-ad.ps1
+++ scripts/create-ad.ps1
@@ -2,5 +2,5 @@
 Add-WindowsFeature -Name AD-Domain-Services, RSAT-AD-PowerShell, DNS, RSAT-DNS-Server
 
 # Enable (and configure) the AD Forest
-$password = ConvertTo-SecureString -String 'Pass@word!' -AsPlainText -Force
+$password = ConvertTo-SecureString -String 'Pass@word1234' -AsPlainText -Force
 Install-ADDSForest -DomainName 'azurelab.cloud' -ForestMode Default -DomainMode Default -InstallDns -SafeModeAdministratorPassword $password -Confirm:$false -DatabasePath 'F:\NTDS' -LogPath 'F:\NTDS' -SysvolPath 'F:\SYSVOL'
