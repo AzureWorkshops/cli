0824520108b456edab899cd3ed493f36a5da504c
Joshua Davis <me@jdav.is>
2017-05-03 14:36:15 -0400

    uncommented out code

--- azworkshops.js
+++ azworkshops.js
@@ -19,13 +19,13 @@ co(function* () {
     let config = new Config();
     
     program.config = yield config.getConfiguration();
-/*
+
     let { credentials, subscriptions } = yield config.login();
     
     program.credentials = credentials;
     program.subscription = yield config.getSubscription(subscriptions);
     program.location = yield config.getLocation(program.credentials, program.subscription);
-*/
+
 }).then(() => {
     return new Promise((resolve, reject) => {
 
--- libs/services/basicActiveDirectory.js
+++ libs/services/basicActiveDirectory.js
@@ -6,17 +6,13 @@ const Config = require('../utils/config');
 const chalk = require('chalk');
 
 module.exports = function (program) {
-    //    let groups = new ResourceGroups(program.credentials, program.subscription.id);
-    let groups = new ResourceGroups('','');
-    //    let exts = new Extensions(program.credentials, program.subscription.id);
+    let groups = new ResourceGroups(program.credentials, program.subscription.id);
+    let exts = new Extensions(program.credentials, program.subscription.id);
     let returnObj = {};
     let config = new Config();
 
-    let name = 'azworkshops_basicAD_04190145459730';
-            returnObj['Resource Group'] = name;
-            
-
     return new Promise(() => { Promise.resolve(); })
+    
         // Get Active Directory Domain
         .then(() => { return config.prompt('Enter a AD domain name:', /^[a-zA-Z0-9]{3,}\.[a-zA-Z]{2,}$/g, 'Must be in the format of \'xxx.xx\'', 'azurelab.cloud'); })
         .then((domain) => { returnObj['Domain Name'] = domain; Promise.resolve(); })
@@ -33,17 +29,17 @@ module.exports = function (program) {
         })
 
         // Create Group
-        .then(() => { return groups.createWithUri('basicAD', program.location.name, 'https://raw.githubusercontent.com/AzureWorkshops/cli/master/templates/basicAD.json', '1.0.2.0'); })
+        .then(() => { return groups.createWithUri('basicAD', program.location.name, 'https://raw.githubusercontent.com/AzureWorkshops/cli/develop/templates/basicAD.json', '1.0.2.0'); })
         .then((group) => { returnObj['Resource Group'] = group; Promise.resolve(); })
 
         // Run Extensions
-        .then(() => { return exts.addPowershellExtension('SetStaticIp', returnObj['Resource Group'], 'dc1', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/master/scripts/set-static-ip.ps1"], 'set-static-ip.ps1', 'Setting static ip on host OS for \'dc1\' VM...'); })
-        .then(() => { return exts.addPowershellExtension('InitializeDisk', returnObj['Resource Group'], 'dc1', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/master/scripts/initialize-single-disk.ps1"], 'initialize-single-disk.ps1', 'Adding \'Data\' (F:) Drive on \'dc1\' VM...'); })
-        .then(() => { return exts.addPowershellExtension('CreateAd', returnObj['Resource Group'], 'dc1', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/master/scripts/create-ad.ps1"], 'create-ad.ps1 -Domain "' + returnObj['Domain Name'] + '" -Netbios "' + returnObj['NETBIOS'] + '"', 'Configuring Domain Controller for \'' + returnObj['Domain Name'] + '\' on \'dc1\' VM...'); })
-        .then(() => { return exts.addPowershellExtension('CreateAdUsers', returnObj['Resource Group'], 'dc1', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/master/scripts/create-ad-users.ps1"], 'create-ad-users.ps1 -Domain "' + returnObj['Domain Name'] + '"', 'Creating demo accounts in \'' + returnObj['Domain Name'] + '\' domain...'); })
-        .then(() => { return exts.addPowershellExtension('SetStaticIp', returnObj['Resource Group'], 'utility', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/master/scripts/set-static-ip.ps1"], 'set-static-ip.ps1 -IPs "10.3.1.4"', 'Setting static ip on host OS for \'utility\' VM...'); })
-        .then(() => { return exts.addPowershellExtension('InitializeDisk', returnObj['Resource Group'], 'utility', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/master/scripts/initialize-single-disk.ps1"], 'initialize-single-disk.ps1', 'Adding \'Data\' (F:) Drive on \'utility\' VM...'); })
-        .then(() => { return exts.addPowershellExtension('AddMachineToAd', returnObj['Resource Group'], 'utility', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/master/scripts/add-machine-to-ad.ps1"], 'add-machine-to-ad.ps1 -Domain "' + returnObj['Domain Name'] + '" -DomainAdmin "' + returnObj['Domain Admin'] + '"', 'Adding \'utility\' VM to \'' + returnObj['Domain Name'] + '\' domain...'); })
+        .then(() => { return exts.addPowershellExtension('SetStaticIp', returnObj['Resource Group'], 'dc1', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/develop/scripts/set-static-ip.ps1"], 'set-static-ip.ps1', 'Setting static ip on host OS for \'dc1\' VM...'); })
+        .then(() => { return exts.addPowershellExtension('InitializeDisk', returnObj['Resource Group'], 'dc1', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/develop/scripts/initialize-single-disk.ps1"], 'initialize-single-disk.ps1', 'Adding \'Data\' (F:) Drive on \'dc1\' VM...'); })
+        .then(() => { return exts.addPowershellExtension('CreateAd', returnObj['Resource Group'], 'dc1', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/develop/scripts/create-ad.ps1"], 'create-ad.ps1 -Domain "' + returnObj['Domain Name'] + '" -Netbios "' + returnObj['NETBIOS'] + '"', 'Configuring Domain Controller for \'' + returnObj['Domain Name'] + '\' on \'dc1\' VM...'); })
+        .then(() => { return exts.addPowershellExtension('CreateAdUsers', returnObj['Resource Group'], 'dc1', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/develop/scripts/create-ad-users.ps1"], 'create-ad-users.ps1 -Domain "' + returnObj['Domain Name'] + '"', 'Creating demo accounts in \'' + returnObj['Domain Name'] + '\' domain...'); })
+        .then(() => { return exts.addPowershellExtension('SetStaticIp', returnObj['Resource Group'], 'utility', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/develop/scripts/set-static-ip.ps1"], 'set-static-ip.ps1 -IPs "10.3.1.4"', 'Setting static ip on host OS for \'utility\' VM...'); })
+        .then(() => { return exts.addPowershellExtension('InitializeDisk', returnObj['Resource Group'], 'utility', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/develop/scripts/initialize-single-disk.ps1"], 'initialize-single-disk.ps1', 'Adding \'Data\' (F:) Drive on \'utility\' VM...'); })
+        .then(() => { return exts.addPowershellExtension('AddMachineToAd', returnObj['Resource Group'], 'utility', program.location.name, ["https://raw.githubusercontent.com/AzureWorkshops/cli/develop/scripts/add-machine-to-ad.ps1"], 'add-machine-to-ad.ps1 -Domain "' + returnObj['Domain Name'] + '" -DomainAdmin "' + returnObj['Domain Admin'] + '"', 'Adding \'utility\' VM to \'' + returnObj['Domain Name'] + '\' domain...'); })
         .then(() => {
             config.printResults(returnObj);
         });
