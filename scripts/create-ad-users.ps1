59b5b3da1d352434e4910a09953f0d3597c136e8
Joshua Davis <me@jdav.is>
2017-05-09 22:20:59 -0400

    updated default password

--- scripts/create-ad-users.ps1
+++ scripts/create-ad-users.ps1
@@ -8,7 +8,7 @@ $jim = 'jim.smith@' + $Domain;
 $sally = 'sally.holly@' + $Domain;
 $richard = 'richard.jackson@' + $Domain;
 
-$password = ConvertTo-SecureString -String 'Pass@word!' -AsPlainText -Force
+$password = ConvertTo-SecureString -String 'Pass@word1234' -AsPlainText -Force
 New-ADUser -Name 'Jim Smith' -GivenName 'Jim' -Surname 'Smith' -SamAccountName 'jim.smith' -UserPrincipalName $jim -AccountPassword $password -PassThru | Enable-ADAccount
 New-ADUser -Name 'Sally Holly' -GivenName 'Sally' -Surname 'Holly' -SamAccountName 'sally.holly' -UserPrincipalName $sally -AccountPassword $password -PassThru | Enable-ADAccount
 New-ADUser -Name 'Richard Jackson' -GivenName 'Richard' -Surname 'Jackson' -SamAccountName 'richard.jackson' -UserPrincipalName $richard -AccountPassword $password -PassThru | Enable-ADAccount
